d9205bb1b95ef31d6a5860a363d755f39b271078
aubinrida <sawaubin@live.fr>
2013-11-14 16:55:06 +0100

    premier commit

--- gestion_projet_db.sql
+++ gestion_projet_db.sql
@@ -1,4 +1,4 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 3.4.10.1
 -- http://www.phpmyadmin.net
 --
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;gchshjszhjedzbnesghhedg
 
 --
 -- Base de données: `gestion_projet_db`
